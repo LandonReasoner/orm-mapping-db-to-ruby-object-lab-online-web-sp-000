de0b4e0926b125abeb50695a2bf56db26c9d1920
Landon Reasoner <reasoner.landon90@gmail.com>
2021-03-14 19:09:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -76,7 +76,7 @@ class Student
     end
   end
   
-  def self.first_X_students_in_grade_10(x)
+  def self.first_X_students_in_grade_10(num)
     sql = <<-SQL
     SELECT * FROM students WHERE students.grade = 10 LIMIT 1
     SQL
