2cbc462865017a59971c77a5521c1db85334ea55
Landon Reasoner <reasoner.landon90@gmail.com>
2021-03-14 19:09:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -81,7 +81,7 @@ class Student
     SELECT * FROM students WHERE students.grade = 10 LIMIT 1
     SQL
     
-    DB[:conn].execute(sql,x).collect do |row|
+    DB[:conn].execute(sql, num).collect do |row|
       self.new_from_db(row)
     end.first 
   end 
